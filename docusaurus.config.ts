9eaaabcd37918fef59371000ff75264110c85083
NotRabbi32 <117670967+NotRabbi32@users.noreply.github.com>
2026-09-24 10:17:59 -0500

    -setup config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,15 +15,15 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://NotRabbi32.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/sandbox-mode-docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'NotRabbi32', // Usually your GitHub org/user name.
+  projectName: 'sandbox-mode-docs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
